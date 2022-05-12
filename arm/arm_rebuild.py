d7fd4e9977f9ef34a59d2a6c84e2ae6478513193
ash256 <1034512163@qq.com>
2022-05-12 14:50:04 +0800

    fix amr rebuild

--- arm/arm_rebuild.py
+++ arm/arm_rebuild.py
@@ -3,53 +3,55 @@
 
 from idaapi import *
 
-def make_func(start, end, pat_str, align):
-	pat = compiled_binpat_vec_t()
-	parse_binpat_str(pat, start, pat_str, 16)
-	
+def find_and_make(start, end, pat, align):
+
 	func_count = 0
-	while(start < end and start != BADADDR):
-		start = bin_search(start, end, pat, BIN_SEARCH_NOSHOW|BIN_SEARCH_FORWARD)
-		if(start == BADADDR):	#找不到pat就结束
+	ea = start
+	while(ea < end):
+		#find_binary(ea, flag, searchstr, radix=16, from_bc695=False)
+		ea = idc.find_binary(ea, SEARCH_DOWN, pat, 16, False)
+		if(ea == BADADDR or ea > end):
 			break
-		elif(get_func_name(start) != None):	#如果是已定义函数，继续查找
-			start = find_func_end(start)
+		
+		if((ea % align) != 0): #不是align字节对齐
+			ea += 1
 			continue
-		elif((start % 4) != 0):	#不是align字节对齐
-			start += 1
+			
+		if(get_func_name(ea) != None):	#如果是已定义函数，继续查找
+			ea = find_func_end(ea)
 			continue
-		else:
-			asm_text = generate_disasm_line(start,GENDSM_FORCE_CODE)
-			is_func = False
-			if(asm_text.find("STMFD") != -1):
-				is_func = True
-			elif(asm_text.find("PUSH") != -1):
-				is_func = True
-			if(is_func):
-				add_func(start)
-				# print("rebuild func: %X" % start)
-				start += 1
-				func_count += 1	
-	print("rebuild function total: %d" % func_count)
+		
+		asm_text = generate_disasm_line(ea, GENDSM_FORCE_CODE)
+		is_func = False
+		if(asm_text.find("STMFD") != -1):
+			is_func = True
+		elif(asm_text.find("PUSH") != -1):
+			is_func = True
+			
+		if(is_func and add_func(ea)):
+			func_count += 1	
+		
+		ea += 1
 
 '''
 	ARM function format:
 	1.  xx xx 2D E9 (STMFD SP!,{Rx-Ry,LR})
 '''
 def make_arm_func(start, end):
-	make_func(start, end, "2D E9 ?? ??", 4)
+	find_and_make(start, end, "?? ?? 2D E9", 4)
 
 '''
 	Thumb function format:
 	1.  xx B5 (push{Rx-Ry,LR})
+	2.  2D E9 xx xx (PUSH.W {R1-R11,LR})
 '''
 def make_thumb_func(start, end):
-	make_func(start, end, "?? B5", 2)
-
+	find_and_make(start, end, "?? B5", 2)
+	find_and_make(start, end, "2D E9 ?? ??", 4)
 
 def main():
-	# make_arm_func(0x80000000,0x80470640)
-	make_arm_func(0x458C440,0x4614614)
+	make_thumb_func(0x0000,0x6000)
+	# make_arm_func(0x44FF000,0x4614614)
 
 if __name__ == '__main__':
 	main()
